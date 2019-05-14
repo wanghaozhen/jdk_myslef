2bbb052e5e8ddb8569719ad2b8231af1447b37ce
wanghz <1533183196@qq.com>
2019-05-14 18:34:32 +0800

    jdk

--- code_string/src/test/java/com/whz/CodeStringApplicationTests.java
+++ code_string/src/test/java/com/whz/CodeStringApplicationTests.java
@@ -12,11 +12,61 @@ import java.io.UnsupportedEncodingException;
 public class CodeStringApplicationTests {
 
 	@Test
-	public void contextLoads() throws UnsupportedEncodingException {
+	public void contextLoads() throws UnsupportedEncodingException, NoSuchFieldException, IllegalAccessException {
 
-		String s = "abc";
-		int a = s.codePointAt(1);
-		System.out.println("a="+a);
+		StringBuilder stringBuilder = new StringBuilder(10);
+		stringBuilder.ensureCapacity(4);
+		int capacity = stringBuilder.capacity();
+		stringBuilder.trimToSize();
+		int length = stringBuilder.length();
+		int capacity2 = stringBuilder.capacity();
+
+//		String str = "vae";
+//		String b = "vae";
+////打印原字符串
+//		System.out.println(str);//vae
+////获取String类中的value字段
+//		Field fieldStr = String.class.getDeclaredField("value");
+////因为value是private声明的，这里修改其访问权限
+//		fieldStr.setAccessible(true);
+////获取str对象上的value属性的值
+//		char[] value = (char[]) fieldStr.get(str);
+////将第一个字符修改为 V(小写改大写)
+//		value[0] = 'V';
+////打印修改之后的字符串
+//		System.out.println(str);//Vae
+//		System.out.println(b);//Vae
+
+//		String s = "aabbccdd";
+//		s = s.replace('a','b');
+
+//		String s = "a,b,c,d,e,f";
+//		String[] a = s.split(",");
+
+//		boolean s = (('7'-'0')|('9'-'7'))<0;
+
+//		String s = "a2c";
+//		int i = s.indexOf(97);
+
+//		String s = "a";
+//		String b = new String("aaaaaaaa");
+//		int i = s.hashCode();
+
+//		String a ="123abc";
+//		String b ="abc";
+//		String d = "abc12abc";
+//		boolean c = a.startsWith(b,3);
+//		boolean e = d.endsWith(b);
+
+//		String s = "abcdefgh";
+//		char chars[] = new char[13];
+//		chars[0]= 'a';
+//		s.getChars(0,7,chars,1);
+
+		//		String s = "abc";
+//		int a = s.codePointAt(1);
+//		System.out.println("a="+a);
+
 //		byte[] abc = s.getBytes("UTF8");
 //		String a = new String(abc,"UTF8");
 //		System.out.println(a);
